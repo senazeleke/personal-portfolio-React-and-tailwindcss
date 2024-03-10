4a5cf61eb360eeb25e42d225eaa590f43137419a
Sena Zeleke <senazeleke258@gmail.com>
2024-03-10 12:13:28 +0300

    minor changes 102

--- portfolio_s/src/App.js
+++ portfolio_s/src/App.js
@@ -3,7 +3,7 @@ import Contact from "./components/Contact";
 import Experience from "./components/Experience";
 import Home from "./components/Home";
 import NavBar from "./components/NavBar";
-import Portfolio from "./components/Portfolio";
+// import Portfolio from "./components/Portfolio";
 import SocialLinks from "./components/SocialLinks";
 
 function App() {
@@ -12,7 +12,7 @@ function App() {
      <NavBar/>
      <Home/>
      <About/>
-     <Portfolio/>
+     {/* <Portfolio/> */}
      <Experience/>     
      <Contact/>
      <SocialLinks/>
